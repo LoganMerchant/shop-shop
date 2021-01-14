e17c3b318f6c1c8f8b0c2217ba5d7f25d021f97c
Logan Merchant <merchantclogan@gmail.com>
2021-01-14 01:03:08 -0700

    remove unneccessary declarations

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useQuery } from "@apollo/react-hooks";
 
 import ProductItem from "../ProductItem";
-import { QUERY_CATEGORIES, QUERY_PRODUCTS } from "../../utils/queries";
+import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
