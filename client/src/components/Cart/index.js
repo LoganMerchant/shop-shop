f483bd1cfcdef400ca3a9b238000ca885760fef7
Logan Merchant <merchantclogan@gmail.com>
2021-01-14 00:01:48 -0700

    implement toggle cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,12 +1,33 @@
 import React from "react";
+
+import "./style.css";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
-import "./style.css";
+import { TOGGLE_CART } from "../../utils/actions";
+import { useStoreContext } from "../../utils/GlobalState";
 
 const Cart = () => {
+  const [state, dispatch] = useStoreContext();
+
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
-      <div className="close">[close]</div>
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
       <h2>Shopping Cart</h2>
       <div>
         <CartItem
